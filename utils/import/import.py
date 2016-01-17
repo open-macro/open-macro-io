a3e7c5fb04d5dc01c529af16ad8079ed7aea75d7
Josh Day <josh@appsaurus.io>
2016-01-17 00:14:22 -0600

    Working import.py script ready to be integrated with excel to json script

--- utils/import/import.py
+++ utils/import/import.py
@@ -9,42 +9,75 @@
 # A MODEL INTO OPEN MACRO. SEE THE README FILE TO SATISFY ALL CONDITIONS
 # NECESSARY TO RUN THIS SCRIPT
 
-import os, sys, argparse
-import mongothon
+import os, sys, argparse, shutil
+from pymongo import MongoClient
 
-env_keys = ['MONGODB_URI','MONGODB_USER', 'MONGODB_PASSWORD']
+MONGODB_URI_KEY = 'MONGODB_URI'
+MONGODB_USER_KEY = 'MONGODB_USER'
+MONGODB_PASSWORD_KEY = 'MONGODB_PASSWORD'
+env_keys = [MONGODB_URI_KEY, MONGODB_USER_KEY, MONGODB_PASSWORD_KEY]
 
-# Ensure that the following conditions are met
-# * Environment keys in env_keys are set
-# * Model excel definition is present
-# * Landing page is present
+# Verify that all mongodb env variables are set
+# Setup MongoDB connection and get the model collection
+def get_model_collection():
+  assert(all(key in os.environ for key in env_keys))
+  uri = os.environ[MONGODB_URI_KEY]
+  user = os.environ[MONGODB_USER_KEY]
+  password = os.environ[MONGODB_PASSWORD_KEY]
+  client = MongoClient(uri)
+  db = client.get_default_database()
+  db.authenticate(user, password)
+  return db['models']
+
+# Verifies Octave script 
 # * Octave scripts are present
 # * Octave scripts have the correct structure
 # * Octave scripts run with default input
-def ensure_env():
-  assert(all(key in os.environ for key in env_keys))
-  return None
+def verify_octave_script(octave_script_dir):
+  print 'Octave script directory: ' + octave_script_dir
 
 # Generate model json to be pushed to mongodb
-def xlsx_to_json():
-  return None
+def xlsx_to_json(xlsx):
+  return {'short_name' : 'test'}
 
-def main(argv):
+def main():
 
+  # setup argparse
   parser = argparse.ArgumentParser()
-  # Setup required input arguments
   parser.add_argument('-s', '--octave-script-dir', required=True,
       help='Directory with octave script for model.')
   parser.add_argument('-l', '--landing-html', required=True,
       help='Landing page html for model.')
   parser.add_argument('-i', '--xlsx-in', required=True,
       help='Excel doc defining model and parameters')
-
   args = parser.parse_args()
 
-  model_xlsx = ensure_env()
-  model_json = xlsx_to_json()
+  # parse the xlsx to get the model json that 
+  # follows the schema defined in mongodb 
+  print 'Creating model.json from ' + args.xlsx_in + '.'
+  model_json = xlsx_to_json(args.xlsx_in)
+  
+  # copy landing page html to landing_pages directory
+  model_short_name = model_json['short_name']
+  dst = '../landing_pages/' + model_short_name + '_landing.html'
+  print 'Copying ' + args.landing_html + ' to ' + dst + '.'
+  shutil.copy(args.landing_html, dst)
 
+  # verify the octave script and copy into run
+  octave_script_dir = args.octave_script_dir
+  print 'Verifying octave script structure.'
+  verify_octave_script(octave_script_dir)
+  dst = '../../run/definitions/' + model_short_name
+  print 'Copying ' + octave_script_dir + ' to ' + dst + '.'
+  shutil.copytree(octave_script_dir, dst)
+
+  # upload model_json to mongodb
+  model_collection = get_model_collection()
+  print 'Uploading generated json to mongodb.'
+  print model_collection.insert_one(model_json)
+
+  print 'Success!'
+  
 if __name__ == "__main__":
-  main(sys.argv[1:])
+  main()
 
